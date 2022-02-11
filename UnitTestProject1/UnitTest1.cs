24660373235749b7be3fbf99b879fe0d697cb37c
IqbalPuteh <99324365+IqbalPuteh@users.noreply.github.com>
2022-02-11 09:14:05 +0800

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var ExpectedPeoClassResult = "Iqbal Puteh";
+            People Peo = new People("Puteh", "Iqbal" + " ");
+            Assert.AreEqual(ExpectedPeoClassResult, Peo.ToString());    
+
+
+        }
+    }
+}
